1b0c58e0b29b7daf91bcc014f3f8c1e2ae6de01d
Rico Bergmann <rico.bergmann1@tu-dresden.de>
2025-07-17 09:38:48 +0200

    Fixes to experiment param specification

--- ari/xctl.py
+++ ari/xctl.py
@@ -6,8 +6,7 @@ import subprocess
 from dataclasses import dataclass
 from datetime import datetime
 from pathlib import Path
-from typing import Literal
-
+from typing import Literal, Optional
 
 import pandas as pd
 
@@ -37,73 +36,126 @@ def log(entry: LogEntry, log_path: str | Path) -> None:
     log_df.to_csv(log_path, mode="a", header=False, index=False)
 
 
-def start_experiment(script: str, args: dict[str, str], switches: list[str]) -> None:
+def start_experiment(
+    script: str,
+    args: Optional[dict[str, str | list[str]]] = None,
+    switches: Optional[list[str]] = None,
+) -> None:
+    args = args or {}
+    switches = switches or []
     absolute_path = Path("/ari/experiments") / script
-    formatted_args = [str(elem) for pair in args.items() for elem in pair]
+
+    formatted_args: list[str] = []
+    for arg, val in args.items():
+        if isinstance(val, list):
+            formatted_args.append(arg)
+            formatted_args.extend(val)
+            continue
+
+        formatted_args.append(arg)
+        formatted_args.append(val)
+
     all_args = formatted_args + switches
     args_str = " ".join(all_args)
 
-    log(LogEntry(script=script, event="start", timestamp=datetime.now(), args=args_str), "/ari/results/progress.log")
+    log(
+        LogEntry(script=script, event="start", timestamp=datetime.now(), args=args_str),
+        "/ari/results/progress.log",
+    )
     subprocess.run(["python3", absolute_path] + all_args)
-    log(LogEntry(script=script, event="end", timestamp=datetime.now(), args=args_str), "/ari/results/progress.log")
+    log(
+        LogEntry(script=script, event="end", timestamp=datetime.now(), args=args_str),
+        "/ari/results/progress.log",
+    )
 
 
 def experiment_native_runtimes(benchmark: Benchmarks) -> None:
     out_dir = ResultsDir / "base"
     out_dir.mkdir(parents=True, exist_ok=True)
 
-    start_experiment("experiment-00-native-runtimes.py", {
-        "--bench": benchmark,
-        "--workloads-dir": "/ari/postbound/workloads",
-        "--db-conn": f"/ari/.psycopg_connection_{benchmark}",
-        "--out": out_dir / f"native-runtimes-{benchmark}.csv",
-    }, ["--prewarm"])
+    start_experiment(
+        "experiment-00-native-runtimes.py",
+        {
+            "--bench": benchmark,
+            "--workloads-dir": "/ari/postbound/workloads",
+            "--db-conn": f"/ari/.psycopg_connection_{benchmark}",
+            "--out": out_dir / f"native-runtimes-{benchmark}.csv",
+        },
+        ["--prewarm"],
+    )
 
 
 def experiment_cardinality_distortion(benchmark: Benchmarks) -> None:
     out_dir = ResultsDir / "experiment-01-cardinality-distortion"
     out_dir.mkdir(parents=True, exist_ok=True)
 
-    start_experiment("experiment-01-cardinality-distortion.py", {
-        "--benchmark": benchmark,
-        "--base-cards": "actual",
-        "--cards-source": f"/ari/datasets/00-base/intermediate-cards-{benchmark}.csv",
-        "--workloads-dir": "/ari/postbound/workloads",
-        "--db-conn": f"/ari/.psycopg_connection_{benchmark}",
-        "--out": out_dir / f"card-distortion-{benchmark}.csv",
-    }, ["--include-vanilla", "--include-default-underest", "--include-default-overest"])
+    start_experiment(
+        "experiment-01-cardinality-distortion.py",
+        {
+            "--benchmark": benchmark,
+            "--base-cards": "actual",
+            "--cards-source": f"/ari/datasets/00-base/intermediate-cards-{benchmark}.csv",
+            "--workloads-dir": "/ari/postbound/workloads",
+            "--db-conn": f"/ari/.psycopg_connection_{benchmark}",
+            "--out": out_dir / f"card-distortion-{benchmark}.csv",
+        },
+        [
+            "--include-vanilla",
+            "--include-default-underest",
+            "--include-default-overest",
+        ],
+    )
 
 
 def experiment_plan_space_analysis(benchmark: Benchmarks) -> None:
     out_dir = ResultsDir / "experiment-03-plan-space-analysis"
     out_dir.mkdir(parents=True, exist_ok=True)
 
-    start_experiment("experiment-03-plan-space-analysis.py", {
-        "--bench": benchmark,
-        "--native-rts": ResultsDir / "base" / f"native-runtimes-{benchmark}.csv",
-        "--cardinalities": f"/ari/datasets/00-base/intermediate-cards-{benchmark}.csv",
-        "--workloads-dir": "/ari/postbound/workloads",
-        "--db-conn": f"/ari/.psycopg_connection_{benchmark}",
-        "--out-dir": out_dir
-    }, [])
+    start_experiment(
+        "experiment-03-plan-space-analysis.py",
+        {
+            "--bench": benchmark,
+            "--native-rts": ResultsDir / "base" / f"native-runtimes-{benchmark}.csv",
+            "--cardinalities": f"/ari/datasets/00-base/intermediate-cards-{benchmark}.csv",
+            "--workloads-dir": "/ari/postbound/workloads",
+            "--db-conn": f"/ari/.psycopg_connection_{benchmark}",
+            "--out-dir": out_dir,
+        },
+        [],
+    )
 
 
 def experiment_architecture_ablation(benchmark: Benchmarks) -> None:
     out_dir = ResultsDir / "experiment-04-beyond-textbook"
     out_dir.mkdir(parents=True, exist_ok=True)
 
-    start_experiment("experiment-07-optimizer-architectures.py", {
-        "--benchmark": benchmark,
-        "--experiments": "native-fixed -- experiments robust-fixed",
-        "--workloads-dir": "/ari/postbound/workloads",
-        "--db-conn": f"/ari/.psycopg_connection_{benchmark}",
-        "--out-dir": out_dir
-    }, ["full"])
+    start_experiment(
+        "experiment-07-optimizer-architectures.py",
+        {
+            "--benchmark": benchmark,
+            "--experiments": ["native-fixed", "robust-fixed"],
+            "--workloads-dir": "/ari/postbound/workloads",
+            "--db-conn": f"/ari/.psycopg_connection_{benchmark}",
+            "--out-dir": out_dir,
+        },
+        ["full"],
+    )
 
 
 def main() -> None:
-    parser = argparse.ArgumentParser(description="Main control script for all experiments.")
-    parser.add_argument("experiment", choices=["all", "0-base", "1-card-distortion", "2-plan-space", "3-beyond-textbook"])
+    parser = argparse.ArgumentParser(
+        description="Main control script for all experiments."
+    )
+    parser.add_argument(
+        "experiment",
+        choices=[
+            "all",
+            "0-base",
+            "1-card-distortion",
+            "3-plan-space",
+            "5-beyond-textbook",
+        ],
+    )
 
     args = parser.parse_args()
 
@@ -113,10 +165,10 @@ def main() -> None:
     if args.experiment in ["all", "1-card-distortion"]:
         experiment_cardinality_distortion("job")
 
-    if args.experiment in ["all", "2-plan-space"]:
+    if args.experiment in ["all", "3-plan-space"]:
         experiment_plan_space_analysis("job")
 
-    if args.experiment in ["all", "3-beyond-textbook"]:
+    if args.experiment in ["all", "5-beyond-textbook"]:
         experiment_architecture_ablation("job")
 
 
