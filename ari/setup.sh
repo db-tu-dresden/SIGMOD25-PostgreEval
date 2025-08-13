5c05845b5f3fbe267624dd36aeb0fc970c12233c
Rico Bergmann <rico.bergmann1@tu-dresden.de>
2025-08-13 12:20:26 +0200

    Move everything ARI-related into the ARI directory

--- ari/setup.sh
+++ ari/setup.sh
@@ -4,7 +4,8 @@ docker build -t ari-elephant .
 
 docker run -dt \
     --name ari-elephant \
-    --volume $PWD/ari-elephant:/ari \
+    --volume $PWD/docker-volume:/ari \
+    --publish 8888:8888 \
     --shm-size=256G \
     -e SETUP_JOB=true \
     ari-elephant
